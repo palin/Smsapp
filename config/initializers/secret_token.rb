f9057961204c791109f8dc11260b02ab0264ced6
Maciek Paliński <maciej.palinski@netguru.pl>
2011-11-25 19:24:46 +0100

    Basic files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Smsapp::Application.config.secret_token = 'e6c19ede4aa6f46ebd615321e4f9af98a7b8e450c450456f2033f4d900c405b303efd5484ea1b20abb1e908fe5d0d0c60e9167df5e6d47e0fd4c54e62fc6481e'
